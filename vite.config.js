2fd8b5bf3ce2b35a21d0d10744dbe78bd3b116f4
James Kabbes <james.kabbes@gmail.com>
2023-09-26 14:23:05 -0500

    setting up dev and prod envs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function getBase(){
+  if (process.env.JAMESKABBES_DEV == 'true'){
+    return ''
+  } else {
+    return '/spotify/'
+  }  
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/spotify/', 
+  base: getBase(), 
   plugins: [react()],
 })
-
-
 
