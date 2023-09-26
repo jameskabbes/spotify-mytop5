dda31dbc1bac1e08fc1f645ba39a953092d9cae0
James Kabbes <james.kabbes@gmail.com>
2023-09-26 09:21:11 -0500

    have authentication working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import spotify_app_config from '../spotify_app_config.json'
+import { Link } from "react-router-dom"
+
 function Home(){
+
+    const tokenPath = `${spotify_app_config.authEndpoint
+        }?client_id=${spotify_app_config.clientId
+        }&redirect_uri=${spotify_app_config.redirectUri
+        }&scope=${spotify_app_config.scopes.join("%20")
+        }&response_type=token&show_dialog=true`
+
+    console.log(tokenPath)
 
     return (
         <>
-        <p>Home page</p>
+            <Link
+                to={tokenPath}
+            >
+                <button
+                >Login to Spotify
+                </button>
+            </Link>
         </>
     )
 
