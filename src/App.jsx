8bcb8a6056ff7ec0ad38a2757ded19ddae978aa4
James Kabbes <james.kabbes@gmail.com>
2023-09-26 12:46:39 -0500

    moving browser router back in

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,19 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home }             from './pages/Home'
 import { PageNotFound }     from './pages/PageNotFound';
 
-
 function App() {
   return (
     <div className="App">
-      <p>Testing!</p>
-    </div>
-  );
-}
-
-export { App };
-
-
-/*
-
-      <BrowserRouter basename='/spotify'>
+      <BrowserRouter>
           <Routes>
             <Route path='/'                       element={<Home/>} />
             <Route path="/404"                    element={<PageNotFound />} />
             <Route path="*"                       element={<Navigate to="/404" />} />
           </Routes>
       </BrowserRouter>
+    </div>
+  );
+}
 
+export { App };
 
-*/
