2c2a52bd8977e230ec6a26b5351e21b47b65c6ae
James Kabbes <james.kabbes@gmail.com>
2023-09-26 14:55:03 -0500

    adding server

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 function getBase(){
   if (process.env.JAMESKABBES_DEV == 'true'){
@@ -11,7 +12,10 @@ function getBase(){
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true
+  },
   base: getBase(), 
-  plugins: [react()],
+  plugins: [react(), tailwindcss('./tailwind.config.js')],
 })
 
