227ed260db89d62a825fb4f20fe0f1f7ff3ce76f
James Kabbes <james.kabbes@gmail.com>
2023-09-28 14:53:13 -0500

    adding spotify green

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "spotify-green": {
+          DEFAULT: "#1DB954"
+        }
+      } 
+    }
   },
   plugins: [],
 }
