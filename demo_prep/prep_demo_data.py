664dab8fe6af321d9248b25a03b3a074d6ba24ee
James Kabbes <james.kabbes@gmail.com>
2023-10-19 17:23:38 -0500

    working to redefine the demo data generation

--- demo_prep/prep_demo_data.py
+++ demo_prep/prep_demo_data.py
@@ -4,32 +4,44 @@ import json
 
 USER_TOKEN = Path('USER_TOKEN.txt').read_text().strip()
 
-artists = [ line.strip() for line in Path( 'artists.txt' ).read_text().split('\n') ]
-tracks =  [ line.strip() for line in Path( 'tracks.txt'  ).read_text().split('\n') ]
+artists = [line.strip() for line in Path('artist.txt').read_text().split('\n')]
+tracks = [line.strip() for line in Path('track.txt').read_text().split('\n')]
 
 artists_data = []
 tracks_data = []
 
-def call_api( search_term, type ):
 
-	return requests.get( 
-		'https://api.spotify.com/v1/search',
-		headers={
-			"Authorization": "Bearer {}".format( USER_TOKEN )
-		},
-		params={
-			'q': search_term,
-			'type': type
-		}
-	)
+def call_api(search_term, type):
+
+    return requests.get(
+        'https://api.spotify.com/v1/search',
+        headers={
+            "Authorization": "Bearer {}".format(USER_TOKEN)
+        },
+        params={
+            'q': search_term,
+            'type': type
+        }
+    )
+
 
 for artist in artists:
-	artists_data.append( call_api( artist, 'artist' ).json()['artists']['items'][0] )
+    res = call_api(artist, 'artist').json()
+    keys = {'name', 'images'}
+    d = {}
+    for key in keys:
+        d[key] = res['artists']['items'][0][key]
+    artists_data.append(d)
 
 for track in tracks:
-	tracks_data.append( call_api( track, 'track' ).json()['tracks']['items'][0] )
+	res = call_api( track, 'track').json()
+    keys = {'name', 'images'}
+	d = {}
+	for key in keys:
+        d[key] = res['tracks']['items'][0][key]
+        
+	tracks_data.append(d)
 
-f = open( '../src/demo.json', 'w' )
-json.dump( { 'artists': artists_data, 'tracks': tracks_data }, f )
+f = open('../src/demo.json', 'w')
+json.dump({'artist': artists_data, 'track': tracks_data}, f)
 f.close()
-
