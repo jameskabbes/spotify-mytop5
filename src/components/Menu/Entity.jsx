24e74ab2c1942a7b8889286fd0f7bbe2f1406ef6
James Kabbes <james.kabbes@gmail.com>
2023-10-19 16:08:25 -0500

    moving to menu section

---
+++ src/components/Menu/Entity.jsx
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+import { EntityContext } from '../../context/EntityContext';
+
+function Entity() {
+
+  const { entity, setEntity } = useContext(EntityContext);
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-center">
+        {entity === 'artist' ? 'Artists' : 'Tracks'}
+      </h1>
+      <div className="text-center">
+        <button
+          onClick={() => setEntity('artist')}
+          className={
+            entity === 'artist' ? 'menu-item-selected' : 'menu-item-deselected'
+          }
+        >
+          Artists
+        </button>
+        <button
+          onClick={() => setEntity('track')}
+          className={
+            entity === 'track' ? 'menu-item-selected' : 'menu-item-deselected'
+          }
+        >
+          Tracks
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export { Entity };
---
+++ src/components/Menu/Limit.jsx
@@ -0,0 +1,23 @@
+function Limit({ limit, setLimit }) {
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setLimit(parseInt(newValue, 10));
+  };
+
+  return (
+    <div className="flex flex-col text-center items-center">
+      <h1>{limit}</h1>
+      <input
+        type="range"
+        min="1"
+        max="50"
+        value={limit}
+        className="slider"
+        id="mySlider"
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export { Limit };
